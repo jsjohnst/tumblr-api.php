a12f838a4474aac1893a34c8c2d0bfcdcdca328d
Jeremy Johnstone <github@jeremyjohnstone.com>
2012-04-15 20:29:49 -0700

    added comment about LSB usage

--- lib/Singleton.php
+++ lib/Singleton.php
@@ -10,6 +10,7 @@ abstract class Singleton
 	
 	protected static function getInstance() {
 		if(is_null(self::$singleton_instance)) {
+			// without using LSB, we would get the wrong class name here (aka Singleton, rather than the derived class)
 			$class_name = get_called_class();
 			self::$singleton_instance = new $class_name();
 		}
